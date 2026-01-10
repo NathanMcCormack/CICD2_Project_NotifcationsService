2dbeb31acc8e7f9686b2180d119cead2c0c0959f
Nathan McCormack <118372005+NathanMcCormack@users.noreply.github.com>
2026-01-10 21:27:26 +0000

    debugging

--- app/database.py
+++ app/database.py
@@ -13,7 +13,7 @@ envfile = {
     "test": ".env.test",
 }.get(os.getenv("APP_ENV", "dev"), ".env.dev")
 
-load_dotenv(envfile, override=True)
+load_dotenv(envfile, override=False)
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 SQL_ECHO = os.getenv("SQL_ECHO", "false").lower() == "true"
